dd515005ad8138a9b4113fa69e259704e2bb15a9
Keith <keith@pebblesandweeds.com>
2024-07-13 12:07:59 -0400

    Add check_utils library for validating matrix multiplication results

--- c/src/main.c
+++ c/src/main.c
@@ -4,13 +4,15 @@
 #include <sys/time.h>
 #include "../include/matmul_lib.h"
 #include "../include/time_utils.h"
+#include "../include/check_utils.h"
 
 int main() {
     float (*A)[N] = malloc(sizeof(float[N][N]));
     float (*B)[N] = malloc(sizeof(float[N][N]));
-    float (*C)[N] = malloc(sizeof(float[N][N]));
+    float (*C_naive)[N] = malloc(sizeof(float[N][N]));
+    float (*C_blocked)[N] = malloc(sizeof(float[N][N]));
 
-    if (A == NULL || B == NULL || C == NULL) {
+    if (A == NULL || B == NULL || C_naive == NULL || C_blocked == NULL) {
         printf("Memory allocation failed\n");
         return 1;
     }
@@ -19,7 +21,8 @@ int main() {
 
     init_matrix(A);
     init_matrix(B);
-    zero_matrix(C);
+    zero_matrix(C_naive);
+    zero_matrix(C_blocked);
 
     // See Python Numpy code comments for FLOP calculation explanation
     long long flop = 2LL * N * N * N;
@@ -27,22 +30,29 @@ int main() {
     printf("%.2f GFLOP\n", flop / 1e9);
 
     // Naive matmul 
-    double time_naive = timed_matmul(matmul, A, B, C);
+    double time_naive = timed_matmul(matmul, A, B, C_naive);
     printf("Naive matmul time taken: %.6f seconds\n", time_naive);
     double gflops_naive = (flop / time_naive) / 1e9;
     printf("Naive matmul: %.2f GFLOPS\n", gflops_naive);
 
     // Matmul with blocks
-    zero_matrix(C);
-
-    double time_blocked = timed_matmul(matmul_blocked, A, B, C);
+    double time_blocked = timed_matmul(matmul_blocked, A, B, C_blocked);
     printf("Blocked matmul time taken: %.6f seconds\n", time_blocked);
     double gflops_blocked = (flop / time_blocked) / 1e9;
     printf("Blocked matmul: %.2f GFLOPS\n", gflops_blocked);
 
+    // Check matrices
+    bool matrices_match = check_matrices(C_naive, C_blocked, 1e-5);
+    if (matrices_match) {
+        printf("Matrices match within tolerance.\n");
+    } else {
+        printf("Matrices do not match within tolerance.\n");
+    }
+
     free(A);
     free(B);
-    free(C);
+    free(C_naive);
+    free(C_blocked);
 
     return 0;
 }
