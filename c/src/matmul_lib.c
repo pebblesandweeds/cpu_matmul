bad6f547b01327c1f14597ca5c428e3f669a7a2f
Keith <keith@pebblesandweeds.com>
2024-07-22 08:21:49 -0400

    Adding vectorized matmul function

--- c/src/matmul_lib.c
+++ c/src/matmul_lib.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+#include <immintrin.h>
 
 // Similar to Numpy but this function will generate a uniform distribution between -1 and 1
 // However, Numpy + Python will generate a normal distribution (bell curve centered at 0) while this function will not
@@ -60,59 +61,41 @@ void matmul_blocked(float A[N][N], float B[N][N], float C[N][N]) {
 }
 
 void matmul_loop_order(float A[N][N], float B[N][N], float C[N][N]) {
-    int i, j, k;
-    register float sum0, sum1, sum2, sum3;
-    register float Aik;
+	int i, j, k;
+    __m256 sum0, sum1, sum2, sum3;
+    __m256 Aik;
     float *bp0_pntr, *bp1_pntr, *bp2_pntr, *bp3_pntr;
-
-    #pragma omp parallel for collapse(2) private(i, j, k, sum0, sum1, sum2, sum3, Aik, bp0_pntr, bp1_pntr, bp2_pntr, bp3_pntr)
-    for (j = 0; j < N; j += 4) {       
-        for (i = 0; i < N; i++) {     
-            sum0 = 0.0;
-            sum1 = 0.0;
-            sum2 = 0.0;
-            sum3 = 0.0;
-
-            bp0_pntr = &B[0][j];
+	#pragma omp parallel for collapse(2) private(i, j, k, sum0, sum1, sum2, sum3, Aik, bp0_pntr, bp1_pntr, bp2_pntr, bp3_pntr)
+	for (j = 0; j < N; j += 4) {
+		for (i = 0; i < N; i++) {
+			sum0 = _mm256_setzero_ps();
+            sum1 = _mm256_setzero_ps();
+            sum2 = _mm256_setzero_ps();
+            sum3 = _mm256_setzero_ps();
+			bp0_pntr = &B[0][j];
             bp1_pntr = &B[0][j + 1];
             bp2_pntr = &B[0][j + 2];
             bp3_pntr = &B[0][j + 3];
-
-            for (k = 0; k < N; k += 4) {  
-                Aik = A[i][k];
-                sum0 += Aik * *bp0_pntr;
-                sum1 += Aik * *bp1_pntr;
-                sum2 += Aik * *bp2_pntr;
-                sum3 += Aik * *bp3_pntr;
-
-                Aik = A[i][k + 1];
-                sum0 += Aik * *(bp0_pntr + N);
-                sum1 += Aik * *(bp1_pntr + N);
-                sum2 += Aik * *(bp2_pntr + N);
-                sum3 += Aik * *(bp3_pntr + N);
-
-                Aik = A[i][k + 2];
-                sum0 += Aik * *(bp0_pntr + 2 * N);
-                sum1 += Aik * *(bp1_pntr + 2 * N);
-                sum2 += Aik * *(bp2_pntr + 2 * N);
-                sum3 += Aik * *(bp3_pntr + 2 * N);
-
-                Aik = A[i][k + 3];
-                sum0 += Aik * *(bp0_pntr + 3 * N);
-                sum1 += Aik * *(bp1_pntr + 3 * N);
-                sum2 += Aik * *(bp2_pntr + 3 * N);
-                sum3 += Aik * *(bp3_pntr + 3 * N);
-
-                bp0_pntr += 4 * N;
-                bp1_pntr += 4 * N;
-                bp2_pntr += 4 * N;
-                bp3_pntr += 4 * N;
-            }
-
-            C[i][j] += sum0;
-            C[i][j + 1] += sum1;
-            C[i][j + 2] += sum2;
-            C[i][j + 3] += sum3;
-        }
-    }
+			for (k = 0; k < N; k++) {
+				Aik = _mm256_set1_ps(A[i][k]);
+				sum0 = _mm256_add_ps(sum0, _mm256_mul_ps(Aik, _mm256_set1_ps(*bp0_pntr)));
+                sum1 = _mm256_add_ps(sum1, _mm256_mul_ps(Aik, _mm256_set1_ps(*bp1_pntr)));
+                sum2 = _mm256_add_ps(sum2, _mm256_mul_ps(Aik, _mm256_set1_ps(*bp2_pntr)));
+                sum3 = _mm256_add_ps(sum3, _mm256_mul_ps(Aik, _mm256_set1_ps(*bp3_pntr)));
+				bp0_pntr += N;
+                bp1_pntr += N;
+                bp2_pntr += N;
+                bp3_pntr += N;
+			}
+			float sums[8];
+            _mm256_storeu_ps(sums, sum0);
+            C[i][j] += sums[0];
+            _mm256_storeu_ps(sums, sum1);
+            C[i][j + 1] += sums[0];
+            _mm256_storeu_ps(sums, sum2);
+            C[i][j + 2] += sums[0];
+            _mm256_storeu_ps(sums, sum3);
+            C[i][j + 3] += sums[0];
+		}
+	}
 }
